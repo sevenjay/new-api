2b6f6266d08001551e654cfb61628a26a9385e60
jay <sevenjay@users.noreply.github.com>
2026-01-20 18:26:18 +0800

    feat: add Token Name filter to dashboard search conditions

--- controller/usedata.go
+++ controller/usedata.go
@@ -14,7 +14,8 @@ func GetAllQuotaDates(c *gin.Context) {
 	startTimestamp, _ := strconv.ParseInt(c.Query("start_timestamp"), 10, 64)
 	endTimestamp, _ := strconv.ParseInt(c.Query("end_timestamp"), 10, 64)
 	username := c.Query("username")
-	dates, err := model.GetAllQuotaDates(startTimestamp, endTimestamp, username)
+	tokenName := c.Query("token_name")
+	dates, err := model.GetAllQuotaDates(startTimestamp, endTimestamp, username, tokenName)
 	if err != nil {
 		common.ApiError(c, err)
 		return
@@ -31,6 +32,7 @@ func GetUserQuotaDates(c *gin.Context) {
 	userId := c.GetInt("id")
 	startTimestamp, _ := strconv.ParseInt(c.Query("start_timestamp"), 10, 64)
 	endTimestamp, _ := strconv.ParseInt(c.Query("end_timestamp"), 10, 64)
+	tokenName := c.Query("token_name")
 	// 判断时间跨度是否超过 1 个月
 	if endTimestamp-startTimestamp > 2592000 {
 		c.JSON(http.StatusOK, gin.H{
@@ -39,7 +41,7 @@ func GetUserQuotaDates(c *gin.Context) {
 		})
 		return
 	}
-	dates, err := model.GetQuotaDataByUserId(userId, startTimestamp, endTimestamp)
+	dates, err := model.GetQuotaDataByUserId(userId, startTimestamp, endTimestamp, tokenName)
 	if err != nil {
 		common.ApiError(c, err)
 		return
